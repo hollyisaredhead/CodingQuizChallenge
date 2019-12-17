4a24cd4ceb77bf3737a32e6353565fe59f92da87
Holly Phelps <Hollyphelps1992@gmail.com>
2019-12-16 21:03:25 -0800

    updated q

--- questions.js
+++ questions.js
@@ -5,23 +5,30 @@ var questions = [
         answer: "alerts"
     },
     {
-        title:"",
-            choices: [""],
-        answer: ""
+        title:"The condition in an if / else statement is enclosed within ",
+            choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
     },
     {
-        title:"",
-            choices: [],
-        answer: ""
-    },
-    {
-        title:"",
-            choices: [],
-        answer: ""
-    },
-    {
-        title:"",
-            choices: [],
-        answer: ""
-    }
-];
+        title: "Arrays in JavaScript can be used to store ____.",
+        choices: [
+          "numbers and strings",
+          "other arrays",
+          "booleans",
+          "all of the above"
+        ],
+        answer: "all of the above"
+      },
+      {
+        title:
+          "String values must be enclosed within ____ when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotes", "parentheses"],
+        answer: "quotes"
+      },
+      {
+        title:
+          "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+        answer: "console.log"
+      }
+    ];
